e7bf3baba5a207caf119c04795ce9745220b762e
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-10 16:00:20 -0400

    Update mpl_np.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Research code/mpl_np.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Research code/mpl_np.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # try opening JSON file (redacted)
-f = open('redacted.json')
+report = open('redacted')
 
 # return JSON object as a dictionary
-data = json.load(f)
+# datadict = json.load(report)
+# print(datadict)
+datanp = np.array(json.load(report))
+print(datanp)
 
 # iterate through JSON list (redacted)
-for i in data["redacted"]:
+for i in datanp['redacted']:
     print(i)
 
-f.close()
+report.close()
 
 # need this if running code on IDLE
 # plt.show() 
