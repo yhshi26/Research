94e3822a74656d124525d653588c09ac17874f62
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-14 14:01:15 -0400

    Update mpl_pd.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Research Code/mpl_pd.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Research Code/mpl_pd.py
@@ -5,14 +5,22 @@ import json
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from matplotlib.dates import DateFormatter
+
 import pandas as pd
 
-# 3815, 3108, 4034
+# handle date time conversions between pandas and matplotlib
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+date_form = DateFormatter("%y-%m-%d")
+
+# (redacted)
 # open JSON file (redacted)
-r = open('Report_Json_Session_Report_20220607T124034[1].json')
+r = open('redacted')
 
 # can't straight-up read JSON object to a DataFrame due to ValueError: All arrays must be of same length
-# df = pd.read_json('Report_Json_Session_Report_20220607T123815[1].json')
+# df = pd.read_json('redacted')
 
 # to combat this we convert the JSON object to a dictionary before converting it to a DataFrame, giving us control
 d = json.load(r)
@@ -35,12 +43,48 @@ for i in df:
 # finding range to tranverse through DataFrame
 # print("df.count() to int: " + str(df.count()))
 
+# graph dark background
+plt.style.use('dark_background')
+
+# create a figure containing a single axes
+fig, ax = plt.subplots() 
+ax.set_title("Report graph (average local field potential values from livestream sessions)") 
+ax.set_xlabel("Date")
+ax.set_ylabel("Local field potential")
+# set limits to number of ticks on x and y axes
+ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+ax.yaxis.set_major_locator(plt.MaxNLocator(11))
+
 # traverse through DataFrame
 # iterate through non-null counts of column sensing_channel and print information
 for i in range(0, int(df.count())):
-    # current = df.iloc[i]
-    # array = current.axes;
-    print(df.iloc[i])
+    # df.iloc[i] dtype: object
+    # parameters: HemisphereLocationDef.Right/Left [{'DateTime', 'LFP'], name
+    current = str(df.iloc[i][0][i]).split(' ')
+    # parsing out parameters
+    date_time = current[1].split("'")
+    # year, month, day
+    # date = date_time[1].split('-')
+    # year = int(date[0])
+    # month = int(date[1])
+    # day = int(date[2].split('T')[0])]
+    # hour, minute, second
+    # time = date[2].split('T')[1]
+    # time = time[0].split(':','Z')
+    # hour = time[0]
+    # minute = time[2]
+    # second = time[4]
+    # local field potential (LFP)
+    local_field_potential = int(current[3].split(',')[0])
+    # amplitude (of deep brain stimulation (DBS))
+    # if (i<int(df.count())):
+    #     amplitude_mA = int(current[5])
+    # else:
+    #     # accounting for end integer with } to parse out
+    #     amplitude_mA = int(current[5].split('}')[0])
+    # plot data on axes
+    date = date_time[1].split('T')[0]
+    ax.plot(date, local_field_potential, linestyle='-', marker='o', c="blue")
 
 # need this if running code on IDLE
-# plt.show() 
+plt.show() 
