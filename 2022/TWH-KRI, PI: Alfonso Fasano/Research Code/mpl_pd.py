61b72c8982c635d148981632a6de3527d532963a
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-14 14:45:45 -0400

    Update mpl_pd.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Research Code/mpl_pd.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Research Code/mpl_pd.py
@@ -15,9 +15,9 @@ register_matplotlib_converters()
 
 date_form = DateFormatter("%y-%m-%d")
 
-# (redacted)
+# 3815, 3108, 4034
 # open JSON file (redacted)
-r = open('redacted')
+r = open('Report_Json_Session_Report_20220607T123108[1].json')
 
 # can't straight-up read JSON object to a DataFrame due to ValueError: All arrays must be of same length
 # df = pd.read_json('redacted')
@@ -25,23 +25,11 @@ r = open('redacted')
 # to combat this we convert the JSON object to a dictionary before converting it to a DataFrame, giving us control
 d = json.load(r)
 
-# indexing through the dictionary to find what index we need
-# for i in d['DiagnosticData']['LFPTrendLogs']:
-#    print(i)
-
-# converting the dictionary to a DataFrame using the indexes we need
+# converting the dictionary to a DataFrame using the indices we need
 df = pd.DataFrame(d['DiagnosticData']['LFPTrendLogs'])
 
 # get information on df
-df.info()
-
-# traverse and print DataFrame
-# iterates over sensing_channel only due to DataFrame form (I think)
-for i in df:
-    sensing_channel = i
-
-# finding range to tranverse through DataFrame
-# print("df.count() to int: " + str(df.count()))
+# df.info()
 
 # graph dark background
 plt.style.use('dark_background')
@@ -63,28 +51,12 @@ for i in range(0, int(df.count())):
     current = str(df.iloc[i][0][i]).split(' ')
     # parsing out parameters
     date_time = current[1].split("'")
-    # year, month, day
-    # date = date_time[1].split('-')
-    # year = int(date[0])
-    # month = int(date[1])
-    # day = int(date[2].split('T')[0])]
-    # hour, minute, second
-    # time = date[2].split('T')[1]
-    # time = time[0].split(':','Z')
-    # hour = time[0]
-    # minute = time[2]
-    # second = time[4]
     # local field potential (LFP)
     local_field_potential = int(current[3].split(',')[0])
-    # amplitude (of deep brain stimulation (DBS))
-    # if (i<int(df.count())):
-    #     amplitude_mA = int(current[5])
-    # else:
-    #     # accounting for end integer with } to parse out
-    #     amplitude_mA = int(current[5].split('}')[0])
+
     # plot data on axes
     date = date_time[1].split('T')[0]
-    ax.plot(date, local_field_potential, linestyle='-', marker='o', c="blue")
+    ax.plot(date, local_field_potential, linestyle='-', marker='.', c="blue")
 
 # need this if running code on IDLE
 plt.show() 
