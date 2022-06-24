68b8bece2c81cf63c4c00d91500dd0c4460acbc6
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-24 14:41:06 -0400

    Update patient.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/patient.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/patient.py
@@ -3,26 +3,30 @@ from contextlib import nullcontext
 import imp
 
 class Patient:
-  def __init__(self, first_name, last_name, gender, diagnosis, implant_date, session):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.gender = gender
-    self.diagnosis = diagnosis
-    self.implant_date = implant_date
-    self.session = session
-    self.diagnostic_data = []
+    def __init__(self, first_name, last_name, gender, diagnosis, implant_date, session):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.diagnosis = diagnosis
+        self.implant_date = implant_date
+        self.session = session
+        self.diagnostic_data = []
+
+    # debugging
+    def __repr__(self):
+        return "<Patient first name: %s, last name: %s, gender: %s, diagnosis: %s, implant date: %s>" % (self.first_name, self.last_name, self.gender, self.diagnosis, self.implant_date)
   
-  def add_data(self, diagnostic_data):
-    self.diagnostic_data.append(diagnostic_data)
+    def add_data(self, diagnostic_data):
+        self.diagnostic_data.append(diagnostic_data)
 
 class Session:
-  def __init__(self, start, end, hemisphere):
-    self.start = start
-    self.end = end
-    self.hemisphere = hemisphere
+    def __init__(self, start, end, hemisphere):
+        self.start = start
+        self.end = end
+        self.hemisphere = hemisphere
 
 class DiagnosticData:
-  def __init__(self, date, local_field_potential, amplitude):
-    self.date = date
-    self.local_field_potential = local_field_potential
-    self.amplitude = amplitude
+    def __init__(self, date, local_field_potential, amplitude):
+        self.date = date
+        self.local_field_potential = local_field_potential
+        self.amplitude = amplitude
