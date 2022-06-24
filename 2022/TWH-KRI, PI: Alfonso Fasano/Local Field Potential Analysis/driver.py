4c38c7979f06fc68321410ab850cb34c0d36f63f
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-24 12:56:44 -0400

    Create driver.py

---
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/driver.py
@@ -0,0 +1,12 @@
+import json
+from parser import *
+
+# open file_names.txt as fn 
+# read fn lines and store as array of str values
+with open('file_names.txt') as fn:
+    file_names = fn.readlines()
+
+# iterate through fn in file_names, call parser with fn as argument
+# file_names stored locally to protect any potential sensitive information
+for fn in file_names:
+    parse(fn.split('\n')[0])
