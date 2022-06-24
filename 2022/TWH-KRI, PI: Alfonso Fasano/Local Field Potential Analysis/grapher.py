9c4e245640b7dabd53bc9af27221a41e9202fede
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-24 15:36:28 -0400

    Create grapher.py

---
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/grapher.py
@@ -0,0 +1,58 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from matplotlib.dates import DateFormatter
+
+import numpy as np
+import pandas as pd
+
+# handle date time conversions between pandas and matplotlib
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+def graph(p): 
+    # graph dark background
+    plt.style.use('dark_background')
+
+    # create a figure containing a single axes
+    fig, ax = plt.subplots() 
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Local field potential")
+    # set limits to number of ticks on x and y axes
+    ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(11))
+
+    # initiate sum
+    sum = 0
+    num_data = 0
+
+    y_data = []
+
+    ax.set_title("Report graph for: " + p.first_name + " " + p.last_name) 
+    for d in p.diagnostic_data:
+        ax.plot(d.date, d.local_field_potential, linestyle='-', marker='.', c="white")
+
+        # add LFP to sum
+        sum += d.local_field_potential
+        num_data += 1
+
+        y_data.append(d.local_field_potential)
+
+    relative_threshold = sum/num_data
+    # print relative_threshold
+    print("Relative threshold: " + str(relative_threshold))
+    ax.axhline(y=relative_threshold, linestyle="-", c="white")
+
+    # finding limits for y-axis (https://stackoverflow.com/questions/11882393/matplotlib-disregard-outliers-when-plotting)    
+    ypbot = np.percentile(y_data, 1)
+    # change yptop percentile to be higher/lower depending on fruequency or extremity of outliers in data
+    # yptop = np.percentile(y_data, 75)
+    yptop = np.percentile(y_data, 99)
+    ypad = 0.2*(yptop - ypbot)
+    y_min = ypbot - ypad
+    y_max = yptop + ypad
+
+    ax.set_ylim([y_min, y_max])
+
+    # need this if running code on IDLE
+    plt.show() 
