874b5af002f4dbe50a568cea0df32e0040049192
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-06-25 18:55:09 -0400

    Update driver.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/driver.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/driver.py
@@ -6,12 +6,9 @@ import matplotlib.pyplot as plt
 from parser import parse, patients
 from grapher import graph
 
-# open file_names.txt as fn 
-# read fn lines and store as array of str values
 with open('file_names.txt') as fn:
     file_names = fn.readlines()
 
-# iterate through fn in file_names, call parser with fn as argument
 # file_names stored locally to protect any potential sensitive information
 for fn in file_names:
     parse(fn.split('\n')[0])
