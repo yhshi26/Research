8d65a26cd8a84f5cf9ebd1971c8bdfbb2d945138
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-07-01 14:36:20 -0400

    Update parser.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/parser.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/parser.py
@@ -10,7 +10,6 @@ def parse(fn):
         d = json.load(fn)
 
     for i in d:
-        # 1. arguments for Patient class
         if i == 'PatientInformation':
             # from PatientInformation: name/surname, gender, diagnosis, implant_date
             # from LeadConfiguration: hemisphere, lead_location
@@ -19,13 +18,11 @@ def parse(fn):
         elif i == 'DeviceInformation':   
             v = d[i]['Initial']
             patient.implant_date = v['ImplantDate']
-        # 2. arguments for Session class
         elif i == 'EventSummary':
             # from EventSummary: start/end date, hemisphere
             hemisphere =  d[i]['LfpAndAmplitudeSummary'][0]['Hemisphere']
             session = Session(d[i]['SessionStartDate'], d[i]['SessionEndDate'], hemisphere)
             patient.session = session
-        # 3. arguments for DiagnosticData class
         elif i == 'DiagnosticData':
             # from DiagnosticData: (LFPTrendLogs: hemisphere): date/time, local_field_potential, amplitude
             v = d[i]['LFPTrendLogs'][hemisphere] # this needs to go one level deeper and iterate
