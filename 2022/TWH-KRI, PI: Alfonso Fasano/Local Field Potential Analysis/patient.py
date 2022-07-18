98a34d15d706a94adcf92acad2070caceff12df0
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-07-18 12:19:14 -0400

    Update patient.py

--- 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/patient.py
+++ 2022/TWH-KRI, PI: Alfonso Fasano/Local Field Potential Analysis/patient.py
@@ -5,26 +5,21 @@ import imp
 patients = []
 
 class Patient:
-    def __init__(self, first_name, last_name, gender, diagnosis, implant_date, session):
+    def __init__(self, first_name, last_name, gender, diagnosis, implant_date):
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
         self.diagnosis = diagnosis
         self.implant_date = implant_date
-        self.session = session
-        self.diagnostic_data = []
-        self.graph = NULL
+        self.sessions = []
+        self.left_diagnostic_data = []
+        self.right_diagnostic_data = []
+        self.graphs = []
 
         patients.append(self)
 
     def __repr__(self):
-        return "<Patient first name: %s, last name: %s, gender: %s, diagnosis: %s, implant date: %s>" % (self.first_name, self.last_name, self.gender, self.diagnosis, self.implant_date)
-  
-    def add_data(self, diagnostic_data):
-        self.diagnostic_data.append(diagnostic_data)
-
-    def set_graph(self, graph):
-        self.graph = graph
+        return "first name: %s, last name: %s, gender: %s, diagnosis: %s, implant date: %s" % (self.first_name, self.last_name, self.gender, self.diagnosis, self.implant_date)
 
 class Session:
     def __init__(self, start, end, hemisphere):
